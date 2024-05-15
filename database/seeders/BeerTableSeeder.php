5b477ca33cbe433e7b798d6a8ed74238075f267a
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-15 14:20:00 +0200

    [add] progetto base

---
+++ database/seeders/BeerTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Beer;
+
+class BeerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
